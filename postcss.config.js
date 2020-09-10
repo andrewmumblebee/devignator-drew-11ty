e198744bb66f8bee482c8b78a6f8f1124bc99a8e
Andrew Harris <aceharris@hotmail.co.uk>
2020-09-10 14:26:04 +0100

    fix: css wasn't being minified

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const postcssConfig = {
   plugins: [require("tailwindcss"), require("autoprefixer")],
 };
+
+if (process.env.NODE_ENV === "production") {
+  postcssConfig.plugins.push(require("cssnano"));
+}
+
+module.exports = postcssConfig;
